e773d36bee5fcc27b949a978070661280af4cc26
James Mwangi K <jameshmwangiy@gmail.com>
2026-02-10 15:13:11 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+bundle config set --local frozen false
+bundle config unset deployment
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
